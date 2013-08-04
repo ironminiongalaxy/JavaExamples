dc13b7dd4b827435b3c2346217be86c312682ad1
ironminion.galaxy <ironminion.galaxy@gmail.com>
2013-08-03 20:21:45 -0700

    Nested TryCatch

--- LearnJ/src/com/dj/example/TryCatch.java
+++ LearnJ/src/com/dj/example/TryCatch.java
@@ -3,19 +3,30 @@ package com.dj.example;
 public class TryCatch {
 
 	public static void main(String[] args) {
-		int d, a, test[] = {1};
+		int d, a, test[] = { 1 };
 		try {
-			d = 1;
+			d = 3;
+			System.out.println("Inside Outer Try block");
 			a = 42 / d;
-			System.out.println("This should be printed");
-			System.out.println(test[5]);
-			System.out.println("This should not be printed");
-			
+			System.out.println("Outer Try block : should be printed");
+			try {
+				if (d == 1) {
+					System.out.println("Inside Inner Try block");
+					d = d / (0);
+				}
+				if (d == 3) {
+					int c[] = { 2 };
+					System.out.println("Inside Inner Try block");
+					c[5] = 100;
+					System.out
+							.println("Inner Try block : should NOT be printed");
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("This is an OutOfBounds in Exception:");
+				e.printStackTrace();
+			}
 		} catch (ArithmeticException e) {
-			System.out.println("This is in Exception:");
-			e.printStackTrace();
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("This an out of bounds Exception:");
+			System.out.println("This an Arithmetic Exception:");
 			e.printStackTrace();
 		}
 		System.out.println("After the exception");
