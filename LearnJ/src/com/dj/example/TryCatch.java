c96b99a487f587409aa362d52e3a33a280587088
ironminion.galaxy <ironminion.galaxy@gmail.com>
2013-08-03 19:48:00 -0700

    Multiple TryCatch Block
    ISSUE:2

--- LearnJ/src/com/dj/example/TryCatch.java
+++ LearnJ/src/com/dj/example/TryCatch.java
@@ -3,13 +3,19 @@ package com.dj.example;
 public class TryCatch {
 
 	public static void main(String[] args) {
-		int d, a;
+		int d, a, test[] = {1};
 		try {
-			d = 0;
+			d = 1;
 			a = 42 / d;
+			System.out.println("This should be printed");
+			System.out.println(test[5]);
 			System.out.println("This should not be printed");
+			
 		} catch (ArithmeticException e) {
 			System.out.println("This is in Exception:");
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("This an out of bounds Exception:");
 			e.printStackTrace();
 		}
 		System.out.println("After the exception");
