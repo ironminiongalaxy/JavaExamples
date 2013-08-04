c88ad007c7ddfa4ba97cb10b5b38a5e57907f1ab
ironminion.galaxy <ironminion.galaxy@gmail.com>
2013-08-03 20:48:20 -0700

    Nested Try with Methods containing TryCatch

--- LearnJ/src/com/dj/example/TryCatch.java
+++ LearnJ/src/com/dj/example/TryCatch.java
@@ -1,35 +1,40 @@
 package com.dj.example;
 
 public class TryCatch {
+	private static int d = 1, a, test[] = { 1 };
 
 	public static void main(String[] args) {
-		int d, a, test[] = { 1 };
+
 		try {
-			d = 3;
 			System.out.println("Inside Outer Try block");
 			a = 42 / d;
 			System.out.println("Outer Try block : should be printed");
-			try {
-				if (d == 1) {
-					System.out.println("Inside Inner Try block");
-					d = d / (0);
-				}
-				if (d == 3) {
-					int c[] = { 2 };
-					System.out.println("Inside Inner Try block");
-					c[5] = 100;
-					System.out
-							.println("Inner Try block : should NOT be printed");
-				}
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("This is an OutOfBounds in Exception:");
-				e.printStackTrace();
-			}
-		} catch (ArithmeticException e) {
-			System.out.println("This an Arithmetic Exception:");
+			divByZero();
+
+		} catch (Exception e) {
+			System.out.println("This an Exception in Outer Nest:");
+			//This will catch as Arithmetic when d=0 and OutOfBounds when d=3
 			e.printStackTrace();
 		}
 		System.out.println("After the exception");
+	}
+
+	private static void divByZero() {
+		try {
+			if (d == 1) {
+				System.out.println("Inside Inner Try block");
+				d = d / (0);
+			}
+			if (d == 3) {
+				int c[] = { 2 };
+				System.out.println("Inside Inner Try block");
+				c[5] = 100;
+				System.out.println("Inner Try block : should NOT be printed");
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("This is an Exception in method:");
+			throw e;
+		}
 	}
 
 }
