969edf765fd2468adc61d0c0b1ccaa2cc82f8ea9
ironminion.galaxy <ironminion.galaxy@gmail.com>
2013-08-07 21:52:06 -0700

    Thread example implementing Runnable (run())

---
+++ LearnJ/src/com/dj/example/NewThread.java
@@ -0,0 +1,25 @@
+package com.dj.example;
+
+public class NewThread implements Runnable {
+	Thread t;
+
+	NewThread() {
+		t = new Thread(this, "New Thread");
+		System.out.println("Child Thread: " + t);
+		t.start();
+		System.out.println("After exiting Child Thread");
+	}
+
+	public void run() {
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Child Thread: " + i);
+				Thread.sleep(500);
+			}
+		} catch (InterruptedException ex) {
+			System.out.println("Child Interrupted");
+		}
+		System.out.println("Exiting Child Thread");
+	}
+
+}
--- LearnJ/src/com/dj/example/ThreadEg.java
+++ LearnJ/src/com/dj/example/ThreadEg.java
@@ -3,6 +3,8 @@ package com.dj.example;
 public class ThreadEg {
 
 	public static void main(String[] args) {
+
+		new NewThread();
 		Thread t = Thread.currentThread();
 		System.out.println("Current Thread Name : " + t.getName());
 		System.out.println("Current Thread: " + t);
@@ -11,13 +13,16 @@ public class ThreadEg {
 
 		try {
 			for (int n = 5; n > 0; n--) {
-				System.out.println("n: " + n);
-				Thread.sleep(1000);
+				System.out.println("Main Thread: " + n);
+				// Ideally the main thread should finish last - so, give it a
+				// time out larger than other threads
+				Thread.sleep(500);
 			}
 		} catch (InterruptedException e) {
 			System.out.println("Main Thread Interrupted");
 			e.printStackTrace();
 		}
+		System.out.println("Main Thread complete");
 	}
 
 }
