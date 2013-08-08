34152e24a7bf91dde6e4ed89a8cdc21c9c8f8948
ironminion.galaxy <ironminion.galaxy@gmail.com>
2013-08-07 23:45:24 -0700

    Thread Eg: Priority, join()

--- LearnJ/src/com/dj/example/NewThread.java
+++ LearnJ/src/com/dj/example/NewThread.java
@@ -2,6 +2,8 @@ package com.dj.example;
 
 public class NewThread implements Runnable {
 	String name;
+	private volatile boolean running = true;
+	int clickCount = 0;
 	Thread t;
 
 	NewThread(String threadName) {
@@ -12,6 +14,14 @@ public class NewThread implements Runnable {
 		System.out.println("After exiting Child Thread");
 	}
 
+	NewThread(int priority, String threadName) {
+		name = threadName;
+		t = new Thread(this, name);
+		System.out.println("Child Thread: " + t.getName());
+		t.start();
+	}
+
+	@Override
 	public void run() {
 		try {
 			for (int i = 5; i > 0; i--) {
@@ -22,5 +32,13 @@ public class NewThread implements Runnable {
 			System.out.println("Child Interrupted");
 		}
 		System.out.println("Exiting Child Thread " + name);
+
+		while (running) {
+			clickCount++;
+		}
+	}
+
+	public void stop() {
+		running = false;
 	}
 }
